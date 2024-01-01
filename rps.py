e78d4c00c2919e0c9e0c624a3bed02cb8402a9be
shubhhh19 <ssoni7735@conestogac.on.ca>
2024-01-01 09:38:11 -0500

    Add all conditions for input and output

--- rps.py
+++ rps.py
@@ -11,4 +11,44 @@ while exit == False:
         print("Game ended")
         exit = True
 
-    
+    if user_input == "rock":
+        if computer_input == "rock":
+            print("Your input is rock")
+            print("Computer input is rock")
+            print("It is a tie!")
+        elif computer_input == "paper":
+            print("Your input is rock")
+            print("Computer input is paper")
+            print("Computer wins!")
+        elif computer_input == "scissors":
+            print("Your input is rock")
+            print("Computer input is scissors")
+            print("You win!")
+        
+    elif user_input == "paper":
+        if computer_input == "rock":
+            print("Your input is paper")
+            print("Computer input is rock")
+            print("You win!")
+        elif computer_input == "paper":
+            print("Your input is paper")
+            print("Computer input is paper")
+            print("It is a tie!")
+        elif computer_input == "scissors":
+            print("Your input is paper")
+            print("Computer input is scissors")
+            print("Computer wins!")
+
+    elif user_input == "scissors":
+        if computer_input == "rock":
+            print("Your input is scissors")
+            print("Computer input is rock")
+            print("Computer wins!")
+        elif computer_input == "paper":
+            print("Your input is scissors")
+            print("Computer input is paper")
+            print("You win!")
+        elif computer_input == "scissors":
+            print("Your input is scissors")
+            print("Computer input is scissors")
+            print("It is a tie!")
